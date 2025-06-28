06ad192e10e32af3132d8ef4f34d19261629dae4
Nicolas <nicolascnagel@gmail.com>
2025-06-28 17:28:03 -0300

    testando funcao pegar um produto

--- tests.py
+++ tests.py
@@ -11,3 +11,20 @@ def test_ola_mundo():
 def test_ola_mundo_json():
     response = client.get("/")
     assert response.json() == {"Olá": "Mundo!"}
+
+def test_listar_produtos_endpoint():
+    response = client.get("/produtos")
+    assert response.status_code == 200
+
+def test_listar_produtos_json():
+    response = client.get("/produtos")
+    assert len(response.json()) == 3
+
+def test_pega_um_produto():
+    response = client.get("/produtos/1")
+    assert response.json() == {
+        "id": 1,
+        "nome": "Smartphone",
+        "descricao": "Um telefone que é inteligente",
+        "preco": 1500.0,
+    }
